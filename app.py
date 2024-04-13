ec6997a3ad8b88a423a46f356f44fbf6498dff04
verymuda <143970386+verymuda@users.noreply.github.com>
2024-04-12 18:04:20 -0700

    added age and working on info endpoint

--- app.py
+++ app.py
@@ -12,18 +12,18 @@ def init_db():
     c.execute('''DROP TABLE IF EXISTS users''')
     
     # Create the users table anew
-    c.execute('''CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, balance INTEGER)''')
+    c.execute('''CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, balance INTEGER, age INTEGER)''')
     
     # Insert the specified user accounts
     user_accounts = [
-        ('Kenneth Cher', 1000),
-        ('Bill Luong', 1000),
-        ('Jessica Leung', 1000),
-        ('Derrick Tran', 1000),
-        ('Tyranno Rex', 1000), ('Stego Sarah', 1000), ('Veloci Raptor', 1000),
-        ('Bronto Bill', 1000), ('Tricera Tops', 1000), ('Ankylo Andy', 1000),
-        ('Ptero Peter', 1000), ('Diplo Dan', 1000), ('Iguano Izzy', 1000),
-        ('Mammothus Maximus', 1000)
+        ('Kenneth Cher', 1000, 20),
+        ('Bill Luong', 1000, 21),
+        ('Jessica Leung', 1000, 22),
+        ('Derrick Tran', 1000, 52),
+        ('Tyranno Rex', 1000, 21), ('Stego Sarah', 1000, 65), ('Veloci Raptor', 1000, 49),
+        ('Bronto Bill', 1000, 43), ('Tricera Tops', 1000, 24), ('Ankylo Andy', 1000, 33),
+        ('Ptero Peter', 1000, 22), ('Diplo Dan', 1000, 39), ('Iguano Izzy', 1000, 53),
+        ('Mammothus Maximus', 1000, 18)
     ]
     
     # Insert all user accounts in a single operation
@@ -71,3 +71,6 @@ def get_user(user_id):
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+#info endpoint
+@app.route('/information', methods=['GET'])
